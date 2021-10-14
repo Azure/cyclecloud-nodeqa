65a90c3fd43605183800ad6c5a4dad8359907c99
Michael Requa <mirequa@microsoft.com>
2021-10-14 12:16:14 -0700

    prune unnecessary test

--- specs/default/cluster-init/scripts/launch_nodeqa.sh
+++ specs/default/cluster-init/scripts/launch_nodeqa.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 vm_size=`jetpack config azure.metadata.compute.vmSize | tr '[:upper:]' '[:lower:]'`
+slurm_resume_timeout=`jetpack config slurm.resume_timeout || 1800`
+
+TMP_FILE=`mktemp`
+jetpack config azure.metadata --json > $TMP_FILE
 
 launch() {
   case "$1" in
         standard_nd96asr_v4)
-            /bin/bash $CYCLECLOUD_SPEC_PATH/files/ndv4-qc.sh 
+            /bin/bash $CYCLECLOUD_SPEC_PATH/files/ndv4-qc.sh $slurm_resume_timeout $TMP_FILE 
         ;;
 
         *)
